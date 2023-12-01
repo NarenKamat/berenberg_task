bbea515a88beb20b753b2f35b27b9a46cb2ce7b9
NarenKamat <152644578+NarenKamat@users.noreply.github.com>
2023-12-01 17:53:23 +0000

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-def get_config(section, variablename):
-	config = configparser.ConfigParser()
-	config.read('config.txt')
-	str_var = config[section][variablename]
-	return str_var
